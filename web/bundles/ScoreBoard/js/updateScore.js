8b555f5ec4fb788cb14c3ddf0b584461175c3484
Tardieu Frederic <tardieu.f@hotmail.fr>
2015-01-23 14:24:20 +0100

    Scrutation Json

--- web/bundles/ScoreBoard/js/updateScore.js
+++ web/bundles/ScoreBoard/js/updateScore.js
@@ -1,14 +1,14 @@
 $(function(){
-
+    var score1 = $("#score1");
+    var score2 = $("#score2");  
 	function maFuction(){
-          $.ajax({
-            method:'get',
-            url:'http://localhost:8080/web/app_dev.php/match',
-            success:function(data){
-              $(".ajax").html($(data).find(".ajax").html());
-            }
+          $.getJSON(match_url).success(function(data){
+            console.log(data);
+            score1.html(data.score1);
+            score2.html(data.score2);
+            //$(".ajax").html($(data).find(".ajax").html());
           });
-	}
+  }
 	var x = setInterval(maFuction, 1000);
 
 

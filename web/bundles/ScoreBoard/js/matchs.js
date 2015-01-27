87b7345f9c507633ffd0d1b8e12a89969f000700
Chris <christopher.rochadias@gmail.com>
2015-01-27 11:39:10 +0100

    Motif Dynamique

--- web/bundles/ScoreBoard/js/matchs.js
+++ web/bundles/ScoreBoard/js/matchs.js
@@ -1,34 +1,30 @@
 jQuery(function(){
 
 	
+	
+	
 	// Récupération variable
 	var score1 = $("#score1");
-
-	
 	var score2 = $("#score2");
-	//alert(score1.html());
-	var i = 0;
-	var j = 0;
-	score1.html(i);
-	score2.html(j);
 	var add1 = $("#more1");
 	var add2 = $("#more2");
 	var less1 = $("#less1");
 	var less2 = $("#less2");
-		nb1=score1.html();
 
 	$("i").click(function(){
 		var btn = $(this).attr('id');
-		$.post('http://localhost:8888/www/Scoreboard/web/app_dev.php/match/2', {'btn':btn}, function(data, textStatus) {
+		$.post('http://localhost:8888/www/Scoreboard/web/app_dev.php/match', {'btn':btn}, function(data, textStatus) {
 
 		}, "json");
 
+
+	})
 
 
 	//Ajoute Score 1 : + 1
 	add1.click(function(){
 
-		
+		var i = parseInt(score1.html());
 		i += 1;
 		score1.html(i);
 
@@ -36,12 +32,14 @@ jQuery(function(){
 
 	//Ajoute Score 2 : + 1
 	add2.click(function(){
+		var j = parseInt(score2.html());
 		j+= 1;	
 		score2.html(j);
 	});
 
 	//Enleve Score 1 : - 1
 	less1.click(function(){
+		var i = parseInt(score1.html());
 		if(i > 0){
 		i -= 1;
 		score1.html(i);
@@ -51,6 +49,7 @@ jQuery(function(){
 
 	//Enleve Score 2 : - 1
 	less2.click(function(){
+		var j = parseInt(score2.html());
 		if(j > 0){
 		j -= 1;
 		score2.html(j);
@@ -58,7 +57,5 @@ jQuery(function(){
 
 	});
 
-
-	});
 
 });

05fefe3f8b01c511b3f418e78c778b4e88b20211
Tardieu Frederic <tardieu.f@hotmail.fr>
2015-02-11 14:48:14 +0100

    Modification demo

--- src/ScoreBoardBundle/Controller/ScoreBoardController.php
+++ src/ScoreBoardBundle/Controller/ScoreBoardController.php
@@ -140,7 +140,7 @@ class ScoreBoardController extends Controller
 			}
 
 			if($request->get('btn')=='btnInit'){
-				$update->setEtat(true);
+				
 				$update->setDuree($request->get('duree')*60);
 				$update->setDureeMatch($update->getDureeMatch() + $request->get('duree')*60);
 				$timeline->setEvent("0 Match begining");
@@ -172,11 +172,7 @@ class ScoreBoardController extends Controller
 	
 }
 
-	public function contactAction()
-	{
-		$content = $this->get('templating')->render('ScoreBoardBundle:Default:contact.html.twig');
-		return new Response($content);
-	}
+
 
 	public function createAction()
 	{
